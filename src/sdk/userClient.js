3cdd27728ac4ee43f02c5cb5d8252c96fa606ee2
Lenn <lennyrm343@outlook.com>
2026-02-13 13:36:07 -0800

    add: simulate sdk error

---
+++ src/sdk/userClient.js
@@ -0,0 +1,78 @@
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function RandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomError(successes, total) {
+  if (Math.random() < successes / total) {
+    console.log("Se genero nuevo error")
+    throw new Error("Random failure")
+  };
+}
+
+
+const usersTable = [
+  {
+    id: 1,
+    name: "John Doe",
+    countryId: 1,
+    position: "Developer",
+    description: "Desktop Developer",
+    avatar: "https://api.dicebear.com/9.x/adventurer/svg?seed=Eliza",
+    email: "john.doe@example.com",
+    phone: "123-456-7890",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    countryId: 2,
+    position: "Designer",
+    description: "Web Designer",
+    avatar: "https://api.dicebear.com/9.x/adventurer/svg?seed=Valentina",
+    email: "jane.doe@example.com",
+    phone: "123-456-7890",
+  },
+  {
+    id: 3,
+    name: "Alice Smith",
+    countryId: 3,
+    position: "Manager",
+    description: "Project Manager",
+    avatar: "https://api.dicebear.com/9.x/adventurer/svg?seed=Alicia",
+    email: "alice.smith@example.com",
+    phone: "123-456-7890",
+  },
+];
+
+export const userClient = {
+  getUsers: async () => {
+    await sleep(RandomInt(1000, 5000));
+    randomError(7,10)
+    return usersTable;
+  },
+  addUser: async (user) => {
+    await sleep(RandomInt(500, 2000));
+    const newUser = { ...user, id: usersTable.length + 1 };
+    usersTable.push(newUser);
+    return newUser;
+  },
+  getUserById: async (id) => {
+    await sleep(RandomInt(500, 2000));
+    const user = usersTable.find((item) => item.id === id);
+    return user;
+  },
+  updateUser: async (user) => {
+    await sleep(RandomInt(500, 2000));
+    const index = usersTable.findIndex((item) => item.id === user.id);
+    usersTable[index] = user;
+    return user;
+  },
+  deleteUser: async (id) => {
+    await sleep(RandomInt(500, 2000));
+    const index = usersTable.findIndex((item) => item.id === id);
+    usersTable.splice(index, 1);
+  },
+};
