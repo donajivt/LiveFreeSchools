8d32e452aeb07e40ae2250840ceed76208162e99
Lenn <lennyrm343@outlook.com>
2026-02-25 15:27:17 -0600

    add: cert

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    https: {
+      key: fs.readFileSync('./localhost.pem'),
+      cert: fs.readFileSync('./localhost.pem')
+    },
+    port: 5173
+  }
 })
