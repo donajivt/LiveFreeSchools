a57d13f3a2e90707a6db80e5350787f446ac2409
donajivt <vaniadonajitorres@gmail.com>
2026-04-06 13:52:54 -0600

    refactor

--- ExampleDonaji/lf_APIs/LiveFree.Beacons.API/LiveFree.Beacons.API.Infrastructure/Persistence/AppDbContext.cs
+++ ExampleDonaji/lf_APIs/LiveFree.Beacons.API/LiveFree.Beacons.API.Infrastructure/Persistence/AppDbContext.cs
@@ -12,5 +12,11 @@ namespace LiveFree.Beacons.API.Infrastructure.Persistence
         public DbSet<BeaconType> BeaconTypes { get; set; }
         public DbSet<BeaconLocation> BeaconLocations { get; set; }
         public DbSet<BeaconEvent> BeaconEvents { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
     }
 }
